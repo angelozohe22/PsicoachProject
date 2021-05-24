1f7d9fbb0bdee6e5e14177d72d32135284e744ad
LuisAngel <luis_angelo21@hotmail.com>
2021-05-23 23:55:18 -0500

    no message

--- app/src/main/java/com/example/psicoachproject/ui/modules/main/activities/viewmodel/AuthViewModel.kt
+++ app/src/main/java/com/example/psicoachproject/ui/modules/main/activities/viewmodel/AuthViewModel.kt
@@ -1,7 +1,35 @@
 package com.example.psicoachproject.ui.modules.main.activities.viewmodel
 
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.liveData
+import com.example.psicoachproject.core.Resource
+import com.example.psicoachproject.domain.repository.auth.AuthRepository
+import kotlinx.coroutines.Dispatchers
+import java.lang.Exception
+
 /**
  * Created by Angelo on 5/23/2021
  */
-class AuthViewModel {
+class AuthViewModel(
+    private val repository: AuthRepository
+): ViewModel() {
+
+    fun signIn(email: String, password: String) = liveData(Dispatchers.IO) {
+        emit(Resource.Loading)
+        try {
+            emit(Resource.Success(repository.signIn(email, password)))
+        }catch (e: Exception){
+            emit(Resource.Failure(e))
+        }
+    }
+
+    fun signUp(name: String, email: String, password: String) = liveData(Dispatchers.IO) {
+        emit(Resource.Loading)
+        try {
+            emit(Resource.Success(repository.signUp(name, email, password)))
+        }catch (e: Exception){
+            emit(Resource.Failure(e))
+        }
+    }
+
 }
